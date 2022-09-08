57c59851a7e7b0029ed4119e79afc807c0876fff
ignaciodlta <ignacio.delatorre4320@alumnos.udg.mx>
2022-09-07 22:46:46 -0500

    Tarea y reto

--- app/src/main/java/delaTorre/Arias/Ignaci/quiz/MainActivity.kt
+++ app/src/main/java/delaTorre/Arias/Ignaci/quiz/MainActivity.kt
@@ -1,12 +1,15 @@
 package delaTorre.Arias.Ignaci.quiz
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.constraintlayout.widget.Placeholder
 import androidx.core.widget.ListViewCompat
 import delaTorre.Arias.Ignaci.quiz.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,42 +24,96 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_google, false),
         Question(R.string.question_colores, false)
     )
-
-    private var currentIndex = 0
-    private var points = 0
+    private var answeredQuestions = mutableListOf<Question>()
+    private var currentIndex: Int = 0
+    private var points: Int = 0
 
     private fun updateQuestion(){
         currentIndex = (currentIndex+1) % questionBank.size
         val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
-    }
-    private fun checkanswer(answer: Boolean){
-        val message = if(questionBank[currentIndex].answer == answer) {
-            R.string.correct_toast
-            points++
-        } else {
-            R.string.incorrect_toast
+        val answerIndex = answeredQuestions.indexOf(questionBank[currentIndex])
+        if (answerIndex != -1){
+            val message = if (answeredQuestions[answerIndex].answer) {
+                getString(R.string.correct_snack) + "\n" + getString(R.string.Points, points)
+            } else {
+                getString(R.string.incorrect_snack) + "\n" + getString(R.string.Points, points)
+            }
+            binding.counterPoints.setText(message)
         }
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-        binding.puntaje.setText(points)
+    }
+    private fun checkanswer(answer: Boolean, view: View){
+        if (answeredQuestions.contains(questionBank[currentIndex])){
+            updateQuestion()
+            return
+        }
+        val message = if(questionBank[currentIndex].answer == answer) {
+            R.string.correct_snack
+        } else {
+            R.string.incorrect_snack
+        }
+        val mySnack = Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
+        answeredQuestions.add(questionBank[currentIndex])
+        if(questionBank[currentIndex].answer == answer) {
+            points += 1
+            mySnack.setBackgroundTint(getColor(R.color.green))
+            answeredQuestions.last().answer = true
+        }else{
+            mySnack.setBackgroundTint(getColor(R.color.red))
+            answeredQuestions.last().answer = false
+        }
+        mySnack.show()
+        binding.counterPoints.setText(getString(R.string.Points, points))
+
+        updateQuestion()
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        binding.questionTextView.setText(getString(R.string.question_welcome))
 
         binding.nextButton.setOnClickListener {
             updateQuestion()
         }
         binding.trueButton.setOnClickListener { view: View ->
-            checkanswer(true)
-            updateQuestion()
+            checkanswer(true, view)
         }
         binding.falseButton.setOnClickListener { view: View ->
-            checkanswer(false)
-            updateQuestion()
+            checkanswer(false, view)
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
--- app/src/main/java/delaTorre/Arias/Ignaci/quiz/Question.kt
+++ app/src/main/java/delaTorre/Arias/Ignaci/quiz/Question.kt
@@ -2,4 +2,4 @@ package delaTorre.Arias.Ignaci.quiz
 
 import androidx.annotation.StringRes
 
-data class Question(@StringRes val textResId: Int, val answer: Boolean)
+data class Question(@StringRes val textResId: Int, var answer: Boolean)
